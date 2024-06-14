c6e4a9f8ca896342abf63ba4eeff8fb4b86670d3
daniel <dscarv27@gmail.com>
2024-06-14 14:55:45 -0300

    add css com tailwind

--- src/App.js
+++ src/App.js
@@ -13,9 +13,36 @@ function App() {
             setRole(e.target.value);
             }}
           />
-            <Employee name="Daniel" role="Dev" />
-            <Employee name="Abby" role={role}/>
-            <Employee name="John" />
+        <div className="flex flex-wrap justify-center">
+            <Employee 
+              name="Julia" 
+              role="Dev" 
+              img="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+            />
+            <Employee 
+              name="Abby" 
+              role={role} 
+              img="https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+            />
+            <Employee 
+              name="John" 
+              img="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+            />
+            <Employee 
+              name="Michael" 
+              role="Dev" 
+              img="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+            />
+            <Employee 
+              name="Stuart" 
+              role={role} 
+              img="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+            />
+            <Employee 
+              name="Mary" 
+              img="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+            />
+          </div>
         </>
       )
       :
