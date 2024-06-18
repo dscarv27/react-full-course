d3b5325daa3c39c861134f7b98233fa4c8c76cde
daniel <dscarv27@gmail.com>
2024-06-18 09:18:03 -0300

    finished edit modal

--- src/App.js
+++ src/App.js
@@ -1,44 +1,62 @@
 import './App.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [role, setRole] = useState('dev');
   const [employees, setEmplyees] = useState(
     [
       {
+        id: 1,
         name:"Julia", 
         role:"Manager", 
         img:"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg" 
       },
       {
+        id: 2,
         name:"Abby", 
         role:"Sr Developer",
         img:"https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg" 
       },
       {
+        id: 3,
         name:"John", 
         role: "Jr Developer",
         img:"https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg" 
       },
       {
+        id: 4,
         name:"Mary",
         role: "Designer", 
         img:"https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg" 
       },
       {
+        id: 5,
         name:"Stuart", 
         role:"Devops", 
         img:"https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg" 
       },
       {
+        id: 6,
         name:"Michael", 
         role:"Engineer", 
         img:"https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg"
       }
     ]
   );
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id === employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+
+      return employee;
+    });
+    setEmplyees(updatedEmployees);
+  }
+
   const showEmployess = true;
   return (
     <div className="App">
@@ -52,10 +70,12 @@ function App() {
            {employees.map((employee) => {
             return (
               <Employee 
-              key={uuidv4()}
+              key={employee.id}
+              id={employee.id}
               name={employee.name} 
               role={employee.role} 
-              img={employee.img} 
+              img={employee.img}
+              updateEmployee={updateEmployee}
               />
             )
            })}
